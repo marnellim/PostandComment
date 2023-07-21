f90194ad30781c9c588488e0edfc30d8ab148546
marnellim <marnellimalingin@gmail.com>
2023-07-21 15:16:15 +0800

    first commit

---
+++ app/Http/Controllers/PostCommentMapController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post_Comment_Map;
+use Illuminate\Http\Request;
+
+class PostCommentMapController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post_Comment_Map $post_Comment_Map)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post_Comment_Map $post_Comment_Map)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post_Comment_Map $post_Comment_Map)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post_Comment_Map $post_Comment_Map)
+    {
+        //
+    }
+}
